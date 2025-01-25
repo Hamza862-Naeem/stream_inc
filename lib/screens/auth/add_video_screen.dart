76888ed3115824477e47f0c6d0691f27b7049c76
Hamza862-Naeem <mhamzanaeem862@gmail.com>
2025-01-26 04:03:35 +0500

    finak commit in master

--- lib/screens/auth/add_video_screen.dart
+++ lib/screens/auth/add_video_screen.dart
@@ -6,85 +6,75 @@ import 'package:stream_inc/constants.dart';
 import 'package:stream_inc/screens/auth/confirm_screen.dart';
 
 class AddVideoScreen extends StatelessWidget {
-  const AddVideoScreen({super.key});
+  const AddVideoScreen({Key? key}) : super(key: key);
 
-  pickVideo(ImageSource src, BuildContext context) async{
+  pickVideo(ImageSource src, BuildContext context) async {
     final video = await ImagePicker().pickVideo(source: src);
-    if(video!= null){
+    if (video != null) {
       Navigator.of(context).push(
-        MaterialPageRoute(builder: (context)=> ConfirmScreen(
-          videoFile: File(video.path),
-          videoPath: video.path,
-
+        MaterialPageRoute(
+          builder: (context) => ConfirmScreen(
+            videoFile: File(video.path),
+            videoPath: video.path,
+          ),
         ),
-        
-        ));
+      );
     }
   }
 
-  showOptionsDialog(BuildContext context){
+  showOptionsDialog(BuildContext context) {
     return showDialog(
       context: context,
-       builder: (context)=> SimpleDialog(
+      builder: (context) => SimpleDialog(
         children: [
           SimpleDialogOption(
-            onPressed: ()=> pickVideo(ImageSource.gallery, context),
-            child: const Row(
-              children: [
+            onPressed: () => pickVideo(ImageSource.gallery, context),
+            child: Row(
+              children: const [
                 Icon(Icons.image),
                 Padding(
                   padding: EdgeInsets.all(7.0),
                   child: Text(
                     'Gallery',
-                    style: const TextStyle(
-                      fontSize: 20,
-                    ),
+                    style: TextStyle(fontSize: 20),
                   ),
-                )
+                ),
               ],
             ),
-
           ),
-            SimpleDialogOption(
+          SimpleDialogOption(
             onPressed: () => pickVideo(ImageSource.camera, context),
-            child: const Row(
-              children: [
+            child: Row(
+              children: const [
                 Icon(Icons.camera_alt),
                 Padding(
                   padding: EdgeInsets.all(7.0),
                   child: Text(
                     'Camera',
-                    style: const TextStyle(
-                      fontSize: 20,
-                    ),
+                    style: TextStyle(fontSize: 20),
                   ),
-                )
+                ),
               ],
             ),
-
           ),
-            SimpleDialogOption(
-            onPressed: ()=> Navigator.of(context).pop(),
-            child: const Row(
-              children: [
+          SimpleDialogOption(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Row(
+              children: const [
                 Icon(Icons.cancel),
                 Padding(
                   padding: EdgeInsets.all(7.0),
                   child: Text(
                     'Cancel',
-                    style: const TextStyle(
-                      fontSize: 20,
-                    ),
+                    style: TextStyle(fontSize: 20),
                   ),
-                )
+                ),
               ],
             ),
-
           ),
-        
-        
         ],
-       ));
+      ),
+    );
   }
 
   @override
@@ -92,20 +82,17 @@ class AddVideoScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: InkWell(
-          onTap: ()=> showOptionsDialog,
+          onTap: () => showOptionsDialog(context),
           child: Container(
             width: 190,
             height: 50,
-            decoration: BoxDecoration(
-              color: buttonColor,
-              
-            ),
+            decoration: BoxDecoration(color: buttonColor),
             child: const Center(
               child: Text(
                 'Add Video',
-                style: const TextStyle(
-                  color: Colors.black,
+                style: TextStyle(
                   fontSize: 20,
+                  color: Colors.black,
                   fontWeight: FontWeight.bold,
                 ),
               ),
